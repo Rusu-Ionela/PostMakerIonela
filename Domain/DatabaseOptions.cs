3eb23812f78be1e4f9df8b0940d417dd28830564
Milana Poting <milana.poting@isa.utm.md>
2025-11-30 18:36:10 +0200

    changed DB Connection String for release

--- Domain/DatabaseOptions.cs
+++ Domain/DatabaseOptions.cs
@@ -6,7 +6,7 @@
         public const string DatabaseConnectionString = @"Data Source=DESKTOP-VTQHJ6A;Initial Catalog=PostMaker;Integrated Security=True;Persist Security Info=False;Pooling=False;Multiple Active Result Sets=False;Connect Timeout=60;Encrypt=False;Trust Server Certificate=True;Command Timeout=0";
 #endif
 #if RELEASE
-        public const string DatabaseConnectionString = @"";
+        public const string DatabaseConnectionString = @"Server=tcp:padti.database.windows.net,1433;Initial Catalog=PostMaker;Persist Security Info=False;User ID=pad;Password=utmisa123!;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
 #endif
     }
 }
