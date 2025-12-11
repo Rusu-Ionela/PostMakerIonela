ecc3159ca09ba5198991710c43df145579ba4744
Rusu Ionela <153896402+Rusu-Ionela@users.noreply.github.com>
2025-12-11 09:17:16 +0200

    Handle DB errors in PostService

--- BLL/Concrete/PostService.cs
+++ BLL/Concrete/PostService.cs
@@ -5,45 +5,61 @@ using Domain;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL.Concrete
 {
     public class PostService : IPostService
     {
         private readonly IPostRepository _postRepository;
+
         public PostService(IPostRepository postRepository)
         {
             _postRepository = postRepository;
         }
+
         public void CreatePost(PostDto dto)
         {
-            var post = new Post()
+            try
             {
-                Author = dto.Author,
-                Content = dto.Content.Replace("\n", "<br />"),
-                Created = dto.Created,
-                Id = dto.Id,
-            };
+                var post = new Post
+                {
+                    Author = dto.Author,
+                    Content = dto.Content.Replace("\n", "<br />"),
+                    Created = dto.Created,
+                    Id = dto.Id,
+                };
 
-            _postRepository.CreatePost(post);
-
+                _postRepository.CreatePost(post);
+            }
+            catch (Exception)
+            {
+                // În Azure, dacă DB nu e configurată corect, evităm să „pice” aplicația.
+                // Într-un proiect real am scrie aici log (Serilog, ILogger etc).
+            }
         }
 
         public IList<PostDto> GetPosts()
         {
-            var posts = _postRepository.GetPosts();
-
-            var dtos = posts.Select(x => new PostDto()
+            try
             {
-                Author = x.Author,
-                Content = x.Content,
-                Created = x.Created,
-                Id = x.Id
-            }).ToList();
+                var posts = _postRepository.GetPosts();
 
-            return dtos;
+                var dtos = posts.Select(x => new PostDto
+                {
+                    Author = x.Author,
+                    Content = x.Content,
+                    Created = x.Created,
+                    Id = x.Id
+                }).ToList();
+
+                return dtos;
+            }
+            catch (Exception)
+            {
+                // Dacă în Azure DB nu este accesibilă → întoarcem listă goală,
+                // iar Home/Index afișează mesajul „No posts...”, nu pagina de eroare.
+                return new List<PostDto>();
+            }
         }
     }
 }
